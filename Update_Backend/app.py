e402f5b6276f81413368b6652eeabbfd6019d794
Anshil Chotara <anshilchotara@gmail.com>
2026-03-10 00:14:18 +0530

    Update app.py File add origins

--- Update_Backend/app.py
+++ Update_Backend/app.py
@@ -1,32 +1,50 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from scripts.predict import predict_stock
 from fastapi.middleware.cors import CORSMiddleware
-
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
+
+# Configuration
+SUPPORTED_STOCKS = ["RELIANCE", "TCS", "ADANI", "GOLD", "SILVER", "SUZLON"]
+
 print("RUNNING UPDATE_BACKEND SERVER")
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:8000/predict?stock=Suzlon",
-        "http://127.0.0.1:5173",
-        "https://ml-stock-prediction.onrender.com",
-        "*"   # keep for safety during dev
-    ],
-    allow_credentials=False,
+        "https://ml-stock-prediction.vercel.app/",
+        "http://localhost:5173/"],  
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+class StockRequest(BaseModel):
+    stock: str
 
-
+@app.get("/")
+def read_root():
+    return {
+        "status": "online",
+        "supported_stocks": SUPPORTED_STOCKS
+    }
 
 @app.get("/predict")
 def get_prediction(stock: str):
+    stock = stock.upper()
     try:
         result = predict_stock(stock)
         return result
     except Exception as e:
-        return {
-            "error": str(e)
-        }
+        return {"error": str(e)}
+
+@app.post("/predict")
+def post_prediction(request: StockRequest):
+    stock = request.stock.upper()
+    try:
+        result = predict_stock(stock)
+        return result
+    except Exception as e:
+        return {"error": str(e)}
