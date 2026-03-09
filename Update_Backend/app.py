e925acd3de41de503d12f3e74e4d8b956fd3f18c
Chotara Anshil <anshilchotara@gmail.com>
2026-03-10 00:26:02 +0530

    Update CORS origins in app.py

--- Update_Backend/app.py
+++ Update_Backend/app.py
@@ -7,9 +7,12 @@ app = FastAPI()
 print("RUNNING UPDATE_BACKEND SERVER")
 app.add_middleware(
     CORSMiddleware,
+
     allow_origins=[
-        "https://ml-stock-prediction.vercel.app/",
-        "http://localhost:5173/"],  
+        "https://ml-stock-prediction.vercel.app",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
