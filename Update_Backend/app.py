cb1b25bd0689e80c4eed1294775fce4cbe8018ef
Chotara Anshil <anshilchotara@gmail.com>
2026-03-10 18:37:51 +0530

    Update CORS settings in FastAPI application

--- Update_Backend/app.py
+++ Update_Backend/app.py
@@ -2,28 +2,20 @@ from fastapi import FastAPI
 from scripts.predict import predict_stock
 from fastapi.middleware.cors import CORSMiddleware
 
-
 app = FastAPI()
 print("RUNNING UPDATE_BACKEND SERVER")
+
 app.add_middleware(
     CORSMiddleware,
-
     allow_origins=[
         "https://ml-stock-prediction.vercel.app",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-    ], 
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-    allow_credentials=False,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-
 
 @app.get("/predict")
 def get_prediction(stock: str):
