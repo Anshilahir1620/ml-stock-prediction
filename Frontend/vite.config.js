8f3106040b824953b7aab0f97b5b7bb045a776c0
Anshil Chotara <anshilchotara@gmail.com>
2026-03-11 03:24:52 +0530

    Frontend Chanages

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   build: {
     commonjsOptions: {
-      include: [/plotly.js-dist-min/, /react-plotly.js/]
+      include: [/node_modules/, /plotly.js-dist-min/, /react-plotly.js/]
     }
   }
 })
